9ab4d103da91196b2207893e412943ce0caa04ff
gokepelemo <goke.pelemo@gmail.com>
2023-10-30 14:14:09 -0500

    add cors module

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const multer = require('multer');
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 require('dotenv').config();
 
 require('./config/database');
 
 const app = express();
+const CLIENTDEVPORT = 5173
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${CLIENTDEVPORT}` }))
 
 app.use(logger('dev'));
 app.use(express.json());
