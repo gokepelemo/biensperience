1bdba562dfc4432c65b8bde974a68b320fe5c8ef
gokepelemo <goke.pelemo@gmail.com>
2023-10-30 14:38:19 -0500

    update env config

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,5 +1,5 @@
 import { sendRequest } from './send-request';
-const BASE_URL = 'https://biensperience.onrender.com/api/users/';
+const BASE_URL = process.env.PRODUCTION ? `/api/users/` : 'https://biensperience.onrender.com/api/users/';
 
 export function signUp(userData) {
     return sendRequest(`${BASE_URL}`, 'POST', userData)
