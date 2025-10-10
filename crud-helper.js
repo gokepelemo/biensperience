c04915001acd6f3afe8e4549866d4ff0aec7d246
Goke Pelemo <goke.pelemo@gmail.com>
2025-10-09 21:42:10 -0500

    remove test scripts

--- crud-helper.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-require('./config/database');
-
-const User = require('./models/user');
-const Destination = require('./models/destination');
-const Experience = require('./models/experience');
